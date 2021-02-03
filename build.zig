4830fc1581003e5c5f1d311928e7b1e84bd9da84
Stephen Gutekanst <stephen@hexops.com>
2021-02-03 14:07:20 -0700

    do not run tests multiple times by accident
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -9,18 +9,6 @@ pub fn build(b: *Builder) void {
     var main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
 
-    var fusefilter_tests = b.addTest("src/fusefilter.zig");
-    fusefilter_tests.setBuildMode(mode);
-
-    var xorfilter_tests = b.addTest("src/xorfilter.zig");
-    xorfilter_tests.setBuildMode(mode);
-
-    var util_tests = b.addTest("src/util.zig");
-    util_tests.setBuildMode(mode);
-
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
-    test_step.dependOn(&fusefilter_tests.step);
-    test_step.dependOn(&xorfilter_tests.step);
-    test_step.dependOn(&util_tests.step);
 }
